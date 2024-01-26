48fedfedcc5ca60cf2c6bc655a576947eaec08ef
MinhLong <longbo1208@gmail.com>
2024-01-26 14:08:54 +0700

    node js basic

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
-  //import
+//import
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
 const app = express();
-
+const mysql = require('mysql2')
 const hostname = process.env.HOST_NAME;
 const port = process.env.PORT || 8888;
 //config template engine ,config static files
@@ -13,6 +13,23 @@ configViewEngine(app);
 
 //Khai bao route
 app.use("/", webRoutes);
+
+//TEST CONNECTION
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '123456',
+  database: 'hoidanit',
+  port: 3307
+})
+
+connection.query(
+  'SELECT * FROM Users u ',
+  function (err, result, fields) {
+    console.log("result", result)
+    console.log("fields", fields)
+  }
+)
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
